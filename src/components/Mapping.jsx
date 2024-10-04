9b1cdaba674872fb28d7dc717546d04c872f432c
Aditya Dhanraj <dhanrajadity201@gmail.com>
2024-10-05 02:25:13 +0530

    main

---
+++ src/components/Mapping.jsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+
+
+
+function Mapping()
+{
+    const DIV=styled.div``;
+    const BUTTON=styled.button``;
+    return (
+        <>
+            <DIV className={"mappping"}>
+                <DIV className={"block"}>
+                    <DIV className={"container"}>
+                        <DIV><BUTTON className={"boxes"} id={"box1"}>city1</BUTTON></DIV>
+                        <DIV><BUTTON className={"boxes"} id={"box2"}>city1</BUTTON></DIV>
+                        <DIV><BUTTON className={"boxes"} id={"box3"}>city1</BUTTON></DIV>
+                        <DIV><BUTTON className={"boxes"} id={"box4"}>city1</BUTTON></DIV>
+                        <DIV><BUTTON className={"boxes"} id={"box5"}>city1</BUTTON></DIV>
+                    </DIV>
+                </DIV>
+            </DIV>
+        </>
+    )
+}
+
+export default Mapping
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
