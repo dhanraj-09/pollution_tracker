8ed045bce0ba3f4c087a28d8011adc7c552e3666
nitin <nitinbharadwaj819@gmail.com>
2024-10-05 01:23:23 +0530

    N

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,8 @@
 import "./Homestyle.css"
+import free from "../assets/free.jpg"
+import free1 from "../assets/free1.png"
+import video1 from "../assets/video1.mp4"
+import video2 from "../assets/video2.mp4"
 
 
 
@@ -9,9 +13,19 @@ function Home()
 {
     return (
         <>
-
+            <div>
+                <nav id={"nav"}>
+                    <img src={free1}>
+                    </img>
+                    <header4>Home</header4>
+                    <header4>Data</header4>
+                    <header4>About</header4>
+                    <header4>Contact Us</header4>
+                </nav>
+                <video
+            </div>
         </>
-    )
+)
 }
 
 export default Home
