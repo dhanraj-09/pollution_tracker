efa36975645c105207f2c62a5f79de993c394b86
nitin <nitinbharadwaj819@gmail.com>
2024-10-05 01:48:46 +0530

    nitin

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import free from "../assets/free.jpg"
 import free1 from "../assets/free1.png"
 import video1 from "../assets/video1.mp4"
 import video2 from "../assets/video2.mp4"
-
+import styled from "styled-components";
 
 
 
@@ -11,19 +11,27 @@ import video2 from "../assets/video2.mp4"
 
 function Home()
 {
+
+    const IMG=styled.img``;
+    const VIDEO=styled.video``;
+
+    const SOURCE=styled.source``;
     return (
         <>
             <div>
                 <nav id={"nav"}>
-                    <img src={free1}>
-                    </img>
+                    <IMG src={free1}>
+                    </IMG>
                     <header4>Home</header4>
                     <header4>Data</header4>
                     <header4>About</header4>
                     <header4>Contact Us</header4>
                 </nav>
-                <video
+                <VIDEO width={"750px"} height={"500px"} autoPlay={true} loop={true}>
+                    <SOURCE src={video1} type={"video/mp4"}></SOURCE>
+                </VIDEO>
             </div>
+            <div id={"main"}
         </>
 )
 }
